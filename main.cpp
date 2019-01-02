f60e5177c08bb63ba5625335c0b3b6b0b4d96e44
dawn <y_minn@nate.com>
2019-01-02 23:58:15 +0900

    add Process class

--- main.cpp
+++ main.cpp
@@ -4,9 +4,31 @@
 
 #include <iostream>
 
+enum State {
+    kWait,
+    kRunning,
+    kTerminated
+};
+
+class Process {
+private:
+    int pid;
+    State currentState = kWait;
+
+public:
+    State getState() {
+        return currentState;
+    }
+
+    void setState(State newState) {
+        currentState = newState;
+    }
+};
+
 int main (void) {
     int iter = 0;
     char command = '\0';
+    Process p;
 
     while (iter != -1) {
         if (iter == 0) std::cout << "Started" << std::endl;
